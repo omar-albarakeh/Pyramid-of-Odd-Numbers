56231fa1097b2df37cb3b9dbdc89c9236d136c2c
Omar AlBarakeh <omaralbarakeh2@gmail.com>
2024-10-26 23:19:17 +0300

    pyramid odd nb using javascript output using console

---
+++ odd-numbers.js
@@ -0,0 +1,11 @@
+function poddnumbers(num) {
+    for (let i = 0; i < num; i++) {
+        let nbs = "";
+    for (let j=1; j < 2*i + 2; j +=2) {
+        nbs += j + " "; 
+    }
+    console.log(nbs.trim());
+}
+}
+const x = parseInt(prompt("ur number:"));
+poddnumbers(x);
